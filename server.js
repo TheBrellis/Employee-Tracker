6cc0bb6ca602bb79b2c629e9ba4ea2271795c22a
TheBrellis <brianroyalellis@gmail.com>
2019-12-26 13:29:51 -0500

    formatted server.js to include express-handlebars

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const exphbs = require("express-handlebars");
 const app = express();
 
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
-
+// handlebars middle-ware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// express middle-ware
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 const routes = require('./routes');
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
