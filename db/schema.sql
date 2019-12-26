7f90e671f32913b53a4cf312fdba77d6dfc77f20
TheBrellis <brianroyalellis@gmail.com>
2019-12-26 13:16:15 -0500

    complete schema.sql and seeds.sql files

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,58 @@
--- Build Database
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
 
+
 CREATE TABLE departments(
+	id INTEGER AUTO_INCREMENT, 
     name VARCHAR(255) NOT NULL,
-    PRIMARY KEY(name)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE roles(
-    name VARCHAR(255) NOT NULL,
-    salary INTEGER NOT NULL,
-    department_name INTEGER NOT NULL,
-    CONSTRAINT fk_department_name FOREIGN KEY (department_name) REFERENCES departments(name) ON DELETE CASCADE,
-    PRIMARY KEY(name)
+	id INTEGER AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
 );
 
+CREATE TABLE managers(
+	id INTEGER AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+    );
 
 CREATE TABLE employees(
     id INTEGER AUTO_INCREMENT,
-    first_name varchar(255) NOT NULL,
-    last_name varchar(255),
-    department_name INTEGER NOT NULL, -- including department
-    CONSTRAINT fk_department_name FOREIGN KEY (department_name) REFERENCES departments(name) ON DELETE CASCADE,
-    PRIMARY KEY(id);
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255),
+    salary INTEGER NOT NULL,
+    role_id INTEGER NOT NULL,
+    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    department_id INTEGER NOT NULL,
+	CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE CASCADE,
+    manager_id INTEGER NOT NULL,
+    CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES managers(id) ON DELETE CASCADE,
+    PRIMARY KEY(id)
 );
-
-SELECT 
-    employees.first_name name as first_name, employees.last_name as last_name, departments.name as department
-
+-- Call for all needed table data
+/*
+SELECT
+employees.id as ID, 
+employees.first_name as first_name, 
+employees.last_name as last_name,
+roles.title as title,
+departments.name as department,
+managers.name as manager,
+salary
+FROM
+employees
+LEFT JOIN
+departments
+ON (employees.department_id = departments.id)
+LEFT JOIN
+roles
+ON (employees.role_id = roles.id)
+LEFT JOIN
+managers
+ON (employees.manager_id = managers.id)
+*/
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,24 @@
-INSERT INTO departments (name) VALUES
-("Sales"),
-("Engineering"),
-("Finance"),
-("Legal");
+INSERT INTO departments (name) VALUES 
+	("SALES"),
+	("ENGINEERING"),
+	("FINANCE"),
+	("LEGAL")
+;
 
-INSERT INTO employees (first_name, last_name, )
+INSERT INTO roles(title) VALUES
+("Sales Lead"),
+("Salesperson"),
+("Lead Engineer"),
+("Software Engineer"),
+("Accountant"),
+("Legal Team Lead"),
+("Lawyer");
+
+INSERT INTO managers(name) VALUES
+("John"),("Karen"),("Larry"), ("Stan");
+
+INSERT INTO employees(first_name, last_name, salary, role_id, department_id, manager_id) VALUES
+("Dan","Shaneesy",100000, 1,1,1),
+("Bill","Brasky",80000, 2,1,1),
+("Dave", "Doble",120000,3,2,2),
+("Jeff", "Jumbah",75000,4,2,2);
