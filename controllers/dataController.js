de466e6ce81f347b8fd3681db20d0337bc0210ca
TheBrellis <brianroyalellis@gmail.com>
2019-12-26 17:34:02 -0500

    table is now empty on page load, when view all is selected, all data from sql db is presented accordingly

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -6,23 +6,25 @@ const employee = require("../models/employees.js");
 
 // Create all routes for iteracting with SQL database
 router.get("/", function (req, res) {
+    /*
     employee.readAll(function (data) {
         let dataObject = {
             employees: data
         };
-    res.render("index", dataObject)
-    });
-    
+        */
+    res.render("index")
 });
+    
 
-/*
+
 router.get("/api/all", function (req, res) {
         employee.readAll(function (data) {
             let dataObject = {
                 employees: data
             };
-        });
         res.render("index", dataObject)
+        });
+        
 });
-*/
+
 module.exports = router;
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,15 +1,6 @@
 $(function() {
     $("#query-all").on("click", function(event) {
-        console.log(event);
-        /*
-        $.ajax({
-            method: "GET",
-            url: "/api/all",
-        }).then(
-            console.log(res)
-        );
-        */
-    })
-
+        window.location.replace("/api/all");
+    });
 
 });
