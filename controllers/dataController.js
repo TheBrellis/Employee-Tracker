661e603f71a9c0cd39fb10faab4348d635c7f621
TheBrellis <brianroyalellis@gmail.com>
2019-12-26 14:11:06 -0500

    set up basic dataController.js in controllers directory to handle routing for api calls

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -6,14 +6,14 @@ const employee = require("../models/employees.js");
 
 // Create all routes for iteracting SQL database
 router.get("/", function(req, res) {
-    employees.readAll(function(data) {
+    employee.readAll(function(data) {
         let dataObject = {
             employees: data
         };
-    })
     console.log(dataObject);
     res.render("index", dataObject);
-})
+    });
+});
 
 
 module.exports = router;
