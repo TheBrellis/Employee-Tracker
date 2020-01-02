7c6d47e9d0967b2375f49f53070380099f9557c5
TheBrellis <brianroyalellis@gmail.com>
2020-01-02 09:56:50 -0500

    added click event for all dropdown items

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,7 +4,10 @@ $(function() {
         window.location.replace("/api/all");
     });
 
-    
+    $("a[data-link = 'form']").on("click", function(event) {
+        console.log(event.target);
+       // window.location.replace(`/api/form/${type}/${set}`)
+    })
 
     $("#query-department").on("click", function(event) {
         event.preventDefault();
