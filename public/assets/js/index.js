944db3deebc73b7a56cf4d4a7d29cce5d2b4b311
TheBrellis <brianroyalellis@gmail.com>
2020-01-02 10:09:34 -0500

    pulling data type and data set element from targeted dropdown element and console logging results

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,8 +5,10 @@ $(function() {
     });
 
     $("a[data-link = 'form']").on("click", function(event) {
-        console.log(event.target);
-       // window.location.replace(`/api/form/${type}/${set}`)
+        let type = $(this).data("type");
+        let set = $(this).data("set");
+        console.log(type,set);
+     //   window.location.replace(`/api/form/${type}/${set}`)
     })
 
     $("#query-department").on("click", function(event) {
